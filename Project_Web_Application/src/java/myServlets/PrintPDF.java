0f1cf47be4ef2e482c5b0c6744514325a472e8e0
Mickaël <mickael_fournier@hotmail.fr>
2015-04-29 22:18:08 +0200

    Correction bug pdf

--- Project_Web_Application/src/java/myServlets/PrintPDF.java
+++ Project_Web_Application/src/java/myServlets/PrintPDF.java
@@ -22,7 +22,9 @@ import com.lowagie.text.Font;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.servlet.ServletOutputStream;
 import modele.Place;
 import modele.Rang;
@@ -45,125 +47,127 @@ public class PrintPDF extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         try {
 
-      String login = request.getParameter("login");
-      String ND = request.getParameter("ND");
-      String NR = request.getParameter("NR");
-      String NT = request.getParameter("NT");
-      String NbP = request.getParameter("NbP");
-      String NomSpectacle = request.getParameter("NomSpectacle");
-      String Date = request.getParameter("Date");
-      String Heure = request.getParameter("Heure");
-      
-      List<Place> places = (List<Place>) request.getAttribute("places");
-      List<Rang> rangs = (List<Rang>) request.getAttribute("rangs");
-      
+            String login = request.getParameter("login");
+            String ND = request.getParameter("ND");
+            String NR = request.getParameter("NR");
+            String NT = request.getParameter("NT");
+            String NbP = request.getParameter("NbP");
+            String NomSpectacle = request.getParameter("NomSpectacle");
+            String Date = request.getParameter("Date");
+            String Heure = request.getParameter("Heure");
 
-      Document document = new Document(PageSize.A6);
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      PdfWriter.getInstance(document, baos);
-      document.open();
-      
-      //définition des polices
-      Font font0 = new Font(Font.COURIER);
-      font0.setStyle(Font.UNDERLINE);
-      font0.setStyle(Font.ITALIC);
-      
-      Font font1 = new Font(Font.COURIER);
+            List<Place> places = (List<Place>) request.getAttribute("places");
+            List<Rang> rangs = (List<Rang>) request.getAttribute("rangs");
+
+            Document document = new Document(PageSize.A6);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PdfWriter.getInstance(document, baos);
+            document.open();
+
+            //définition des polices
+            Font font0 = new Font(Font.COURIER);
+            font0.setStyle(Font.UNDERLINE);
+            font0.setStyle(Font.ITALIC);
+
+            Font font1 = new Font(Font.COURIER);
       //font1.setStyle(Font.BOLD);
-      
-      Font font3 = new Font(Font.COURIER);
-      font3.setStyle(Font.BOLD);
-      font3.setStyle(Font.ITALIC);
-      
-      Font font4; 
-      font4 = new Font(Font.COURIER, 8, Font.BOLDITALIC);
-                    
-      //paragraphe
-      Paragraph paragraphNT = new Paragraph();
-      paragraphNT.setFont(font0);
-      paragraphNT.add("Ticket n°" + NT);
-      paragraphNT.setAlignment(Element.ALIGN_CENTER);
-      document.add(paragraphNT);    
-      document.add(Chunk.NEWLINE);
-      
-      //paragraphe
-      Paragraph paragraphND = new Paragraph();
-      paragraphND.setFont(font1);
-      paragraphND.add("Dossier n°" + ND);
-      //paragraphND.setAlignment(Element.ALIGN_CENTER);
-      document.add(paragraphND);    
-      
-      //paragraphe
-      Paragraph paragraphNR = new Paragraph();
-      paragraphNR.setFont(font1);
-      paragraphNR.add("Réservation n°" + NR);
-      document.add(paragraphNR);
-      document.add(Chunk.NEWLINE);
-      
-      //paragraphe
-      Paragraph paragraphLogin = new Paragraph();
-      paragraphLogin.setFont(font1);
-      paragraphLogin.add("Votre login : " + login);      
-      document.add(paragraphLogin);    
-      
-      //paragraphe
-      Paragraph paragraphNbP = new Paragraph();
-      paragraphNbP.setFont(font1);
-      paragraphNbP.add("Nombre de places réservées : " + NbP);
-      document.add(paragraphNbP);    
-      document.add(Chunk.NEWLINE);
-      
-      //paragraphe
-      Paragraph paragraphNomSpectacle = new Paragraph();
-      paragraphNomSpectacle.setFont(font3);
-      paragraphNomSpectacle.add(NomSpectacle);
-      document.add(paragraphNomSpectacle);    
-      
-      //paragraphe
-      Paragraph paragraphDate = new Paragraph();
-      paragraphDate.setFont(font1);
-      paragraphDate.add(Date);
-      paragraphDate.add(" à " + Heure);
-      document.add(paragraphDate);    
-      document.add(Chunk.NEWLINE);
-      
-    //affichage des places
-    if (!places.isEmpty()) {
-        Paragraph paragraphPlaces = new Paragraph();
-        paragraphPlaces.setFont(font4);
-        for (int i = 0; i < places.size(); i++){
-            String NumPl = Integer.toString(places.get(i).getNumPl());
-            String NRa = Integer.toString(places.get(i).getNRa());
-            String PrixCT = Integer.toString(rangs.get(i).getPrixCT());
-            paragraphPlaces.add("Place n°" + NumPl);    
-            paragraphPlaces.add(" Rang n°" + NRa);
-            paragraphPlaces.add(" Prix de la place : " + PrixCT);
+
+            Font font3 = new Font(Font.COURIER);
+            font3.setStyle(Font.BOLD);
+            font3.setStyle(Font.ITALIC);
+
+            Font font4;
+            font4 = new Font(Font.COURIER, 8, Font.BOLDITALIC);
+
+            //paragraphe
+            Paragraph paragraphNT = new Paragraph();
+            paragraphNT.setFont(font0);
+            paragraphNT.add("Ticket n°" + NT);
+            paragraphNT.setAlignment(Element.ALIGN_CENTER);
+            document.add(paragraphNT);
+            document.add(Chunk.NEWLINE);
+
+            //paragraphe
+            Paragraph paragraphND = new Paragraph();
+            paragraphND.setFont(font1);
+            paragraphND.add("Dossier n°" + ND);
+            //paragraphND.setAlignment(Element.ALIGN_CENTER);
+            document.add(paragraphND);
+
+            //paragraphe
+            Paragraph paragraphNR = new Paragraph();
+            paragraphNR.setFont(font1);
+            paragraphNR.add("Réservation n°" + NR);
+            document.add(paragraphNR);
+            document.add(Chunk.NEWLINE);
+
+            //paragraphe
+            Paragraph paragraphLogin = new Paragraph();
+            paragraphLogin.setFont(font1);
+            paragraphLogin.add("Votre login : " + login);
+            document.add(paragraphLogin);
+
+            //paragraphe
+            Paragraph paragraphNbP = new Paragraph();
+            paragraphNbP.setFont(font1);
+            paragraphNbP.add("Nombre de places réservées : " + NbP);
+            document.add(paragraphNbP);
+            document.add(Chunk.NEWLINE);
+
+            //paragraphe
+            Paragraph paragraphNomSpectacle = new Paragraph();
+            paragraphNomSpectacle.setFont(font3);
+            paragraphNomSpectacle.add(NomSpectacle);
+            document.add(paragraphNomSpectacle);
+
+            //paragraphe
+            Paragraph paragraphDate = new Paragraph();
+            paragraphDate.setFont(font1);
+            paragraphDate.add(Date);
+            paragraphDate.add(" à " + Heure);
+            document.add(paragraphDate);
+            document.add(Chunk.NEWLINE);
+
+            Set hashNP = new HashSet();
+
+            //affichage des places
+            if (!places.isEmpty()) {
+                Paragraph paragraphPlaces = new Paragraph();
+                paragraphPlaces.setFont(font4);
+                for (int i = 0; i < places.size(); i++) {
+                    if (!hashNP.contains(places.get(i).getNP())) {
+                        String NumPl = Integer.toString(places.get(i).getNumPl());
+                        String NRa = Integer.toString(places.get(i).getNRa());
+                        String PrixCT = Integer.toString(rangs.get(i).getPrixCT());
+                        paragraphPlaces.add("Place n°" + NumPl);
+                        paragraphPlaces.add(" Rang n°" + NRa);
+                        paragraphPlaces.add(" Prix de la place : " + PrixCT);
+                        hashNP.add(places.get(i).getNP());
+                    }
+                }
+                document.add(paragraphPlaces);
+            }
+
+            document.close();
+
+            response.setHeader("Expires", "0");
+            response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+            response.setHeader("Pragma", "public");
+
+            response.setContentType("application/pdf");
+
+            response.setContentLength(baos.size());
+
+            ServletOutputStream out = response.getOutputStream();
+            baos.writeTo(out);
+            out.flush();
+
+        } catch (DocumentException | IOException e2) {
+            System.out.println("Error in " + getClass().getName() + "\n" + e2);
         }
-        document.add(paragraphPlaces);
-    }
-      
-      
-      
-      document.close();
-
-      response.setHeader("Expires", "0");
-      response.setHeader("Cache-Control","must-revalidate, post-check=0, pre-check=0");
-      response.setHeader("Pragma", "public");
-
-      response.setContentType("application/pdf");
-
-      response.setContentLength(baos.size());
-
-      ServletOutputStream out = response.getOutputStream();
-      baos.writeTo(out);
-      out.flush();
-
-    } catch (DocumentException | IOException e2) {
-      System.out.println("Error in " + getClass().getName() + "\n" + e2);
-    }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
